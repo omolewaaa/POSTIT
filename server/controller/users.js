f551b1c05f22a4034d3219c0185a9e513e385523
omolewa <akinyomiomolewa30gmail.com>
2017-06-15 16:51:09 +0100

    made changes

--- server/controller/users.js
+++ server/controller/users.js
@@ -15,44 +15,5 @@ module.exports = {
 
 
 
-/*module.exports = {
-  findOne(req, res) {
-  users.findOne({ where:{
-    username: req.body.username
-  },
-  }, function(err, users) {
-
-    if (err) throw err;
-
-    if (!users) {
-      res.json({ success: false, message: 'Authentication failed. User not found.' });
-    } else if (users) {
-
-       check if password matches
-      if (users.password !== req.body.password) {
-        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-      } else {
-
-        if user is found and password is right
-        create a token
-        var token = jwt.sign(users, app.get('1103'), {
-          expiresInMinutes: 1440 
-        });
-
-        return the information including token as JSON
-        res.json({
-          success: true,
-          message: 'Enjoy your token!',
-          token: token
-        });
-      }   
-
-    }
-
-  });
-};
-
-*/
-
 
 
