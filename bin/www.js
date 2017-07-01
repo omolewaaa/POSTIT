6d3c92f1009fb9a84964a07618518e360c4aaf48
omolewa <akinyomiomolewa30gmail.com>
2017-07-01 11:43:43 +0100

    heroku deployment

--- bin/www.js
+++ bin/www.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('../app'); // The express app we just created
 //const config = require('../config/config.json');
 
-const port = parseInt(process.env.PORT, 10) || 8000;
+const port = process.env.PORT || 8000;
 //app.set('superSecret', config.secret);
 //app.set('port', port);
 
