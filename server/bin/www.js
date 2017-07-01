8bd0270052b34f6fa60a2d8bec7c5804a86d516d
omolewa <akinyomiomolewa30gmail.com>
2017-07-01 09:47:42 +0100

    heroku deployment

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,16 +1,10 @@
 const http = require('http');
 const app = require('../app'); // The express app we just created
 //const config = require('../config/config.json');
-const port = process.env.PORT || 8000;
 
-app.listen(port, '192.168.43.196' || 'localhost', () => {
-  console.log(`Server started on port ${port}`);
-});
-
-/*const port = parseInt(process.env.PORT, 10) || 8000;
+const port = parseInt(process.env.PORT, 10) || 8000;
 //app.set('superSecret', config.secret);
 app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port);
-*/
