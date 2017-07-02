6e24bc10ea1e71cafafcf5ddb8e0b69fc049853c
omolewa <akinyomiomolewa30gmail.com>
2017-07-02 05:15:21 +0100

    heroku credentials

--- bin/www.js
+++ bin/www.js
@@ -4,8 +4,12 @@ const app = require('../app'); // The express app we just created
 
 //db.sequelize.sync().then(()=>{
 const port = process.env.PORT || 8000;
-app.set('port', port);
+//app.set('port', port);
 
 const server = http.createServer(app);
-server.listen(port);
+//server.listen(port);
 //})
+
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
