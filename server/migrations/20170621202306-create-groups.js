1c00c2daf8d107fd40e8231bb91a13e68d9d91c8
omolewa <akinyomiomolewa30gmail.com>
2017-07-02 06:26:52 +0100

    modified the tables

--- server/migrations/20170621202306-create-groups.js
+++ server/migrations/20170621202306-create-groups.js
@@ -1,7 +1,7 @@
-'use strict';
+
 module.exports = {
-  up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('groups', {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.createTable('groups', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,7 +25,7 @@ module.exports = {
       }
     });
   },
-  down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('groups');
+  down: queryInterface => {
+    queryInterface.dropTable('groups');
   }
 };
--- server/migrations/20170624171445-create-message.js
+++ server/migrations/20170624171445-create-message.js
@@ -1,7 +1,7 @@
-'use strict';
+
 module.exports = {
-  up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('messages', {
+  up: (queryInterface, Sequelize)  => {
+    queryInterface.createTable('messages', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -24,7 +24,7 @@ module.exports = {
       }
     });
   },
-  down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('messages');
+  down: queryInterface => {
+    queryInterface.dropTable('messages');
   }
 };
