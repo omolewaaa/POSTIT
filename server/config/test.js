93c419e889bb0c802ed32f9c7b8c8e8350cb8cad
omolewa <akinyomiomolewa30gmail.com>
2017-07-13 21:31:25 +0100

    working on travis build to run tests

---
+++ server/config/test.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "env": "test",
+    "username": "postgres",
+    "password": "omolewa1993",
+    "database": "post_it",
+    "host": "127.0.0.1",
+    "port": 5432,
+    "dialect": "postgres"
+};
